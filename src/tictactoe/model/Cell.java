b745edf1ef4f72320d7837d8041c473574eb4846
immagixe <immagixe@gmail.com>
2022-04-26 20:41:51 +0300

    Add model classes

---
+++ src/tictactoe/model/Cell.java
@@ -0,0 +1,21 @@
+package tictactoe.model;
+
+public final class Cell {
+
+    private final int row;
+
+    private final int col;
+
+    public Cell(final int row, final int col) {
+        this.row = row;
+        this.col = col;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getCol() {
+        return col;
+    }
+}
--- src/tictactoe/model/GameTable.java
+++ src/tictactoe/model/GameTable.java
@@ -2,4 +2,21 @@ package tictactoe.model;
 
 public class GameTable {
 
+    private final char[][] table = {
+            {' ', ' ', ' '},
+            {' ', ' ', ' '},
+            {' ', ' ', ' '}
+    };
+
+    public boolean isEmpty(final Cell cell) {
+        return table[cell.getRow()][cell.getCol()] == ' ';
+    }
+
+    public char getSign(final Cell cell) {
+        return table[cell.getRow()][cell.getCol()];
+    }
+
+    public void setSign(final Cell cell, final char sign) {
+        table[cell.getRow()][cell.getCol()] = sign;
+    }
 }
