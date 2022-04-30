71048015a51c4848d0453e71585421e313113e1b
immagixe <immagixe@gmail.com>
2022-04-30 23:19:47 +0300

    Implement DrawVerifier component

--- src/tictactoe/component/DrawVerifier.java
+++ src/tictactoe/component/DrawVerifier.java
@@ -1,10 +1,18 @@
 package tictactoe.component;
 
+import tictactoe.model.Cell;
 import tictactoe.model.GameTable;
 
 public class DrawVerifier {
 
     public boolean isDraw(final GameTable gameTable) {
-        return false;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (gameTable.isEmpty(new Cell(i,j))) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }
